302a5baa9cc5d58b166c560bd2a471e42e594ae6
ziliwalid <walidzili19@gmail.com>
2024-02-18 20:26:01 +0000

    commit

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 starts a Flask web application
@@ -24,6 +23,22 @@ def hbnb():
 def cisfun(text):
     """shows c followed by val of var"""
     return 'C ' + text.replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """shows some stuff (explained in the task)"""
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """shows something if n is a num"""
+    return "{:d} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numsTemp(n):
+    """does some displaying"""
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+starts a Flask web app
 """
 
 from flask import Flask
@@ -29,6 +29,26 @@ def cisfun(text):
 def pythoniscool(text='is cool'):
     """shows some stuff (explained in the task)"""
     return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """shows something if n is a num"""
+    return "{:d} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numsTemp(n):
+    """does some displaying"""
+    return render_template('5-number.html', n=n)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def checkifEVEN(n):
+    """display a HTML page only if n is an integer"""
+    if n % 2 == 0:
+        isEven = 'even'
+    else:
+        isEven = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n,
+                           isEven=isEven)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
